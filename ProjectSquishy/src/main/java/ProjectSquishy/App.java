88e8d87b839e127a8aba8df82f3aa403fddfa59a
MatthewHilleryJCU <matthew.hillery@my.jcu.edu.au>
2019-07-13 19:29:08 +1000

    OPtimized user input

--- ProjectSquishy/src/main/java/ProjectSquishy/App.java
+++ ProjectSquishy/src/main/java/ProjectSquishy/App.java
@@ -15,7 +15,6 @@ import ProjectSquishy.utils.parser.ParserController;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -48,40 +47,50 @@ public class App {
 
         Scanner s = new Scanner(System.in);
         System.out.println("\n Welcome to ProjectSquishy! \n");
-        printOptions();
-        String pick = s.nextLine();
-        List<Player> searchedPlayers = new ArrayList<>();
+        int pick = printMenu(s);
+        List<Player> searchedPlayers;
 
-        if (!(pick.equals("1") || pick.equals("2") || pick.equals("3"))) {
-            System.out.println("Pick a valid number.");
-            printOptions();
-        } else if (pick.equals("1")) {
-            searchedPlayers = playerDao.getAll();
+
+        while (pick != 9) {
+            pick = printMenu(s);
+            switch (pick) {
+                case 1:
+                    searchedPlayers = playerDao.getAll();
 //            System.out.println(searchedPlayers);
-            for (Player player : searchedPlayers) {
-                System.out.println(player.getPlayerName());
+                    for (Player player : searchedPlayers) {
+                        System.out.println(player.getPlayerName());
+                    }
+                    break;
+                case 2:
+                    System.out.println("What player?");
+                    String playerChoice = s.nextLine();
+                    searchedPlayers = playerDao.getAllSearch(playerChoice);
+                    System.out.println(searchedPlayers);
+                    break;
+                case 3:
+                    players = parserController.parsePlayerData();
+                    playerDataController.print(players);
+                    daoController.addAllPlayers(players, playerDao, controlDao, cameraDao, deadzoneDao);
+                    break;
+                case 9:
+                    break;
+                default:
+                    System.out.println("Sorry, please enter valid Option");
             }
-
-        } else if (pick.equals("2")) {
-            System.out.println("What player?");
-            String playerChoice = s.nextLine();
-            searchedPlayers = playerDao.getAllSearch(playerChoice);
-            System.out.println(searchedPlayers);
-        } else {
-            players = parserController.parsePlayerData();
-            playerDataController.print(players);
-            daoController.addAllPlayers(players, playerDao, controlDao, cameraDao, deadzoneDao);
         }
+
 
         // Close Entity Manager Factory
         emf.close();
     }
 
 
-    private static void printOptions() {
+    private static int printMenu(Scanner s) {
         System.out.println("(1) Show all players settings");
         System.out.println("(2) Choose a players settings");
         System.out.println("(3) Update player data");
+        System.out.println("(9) Quit");
+        return Integer.valueOf(s.nextLine());
     }
 
 
