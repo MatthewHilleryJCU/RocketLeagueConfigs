d1d6e1f83570379cdf3b81fd42276311fce42672
MatthewHilleryJCU <matthew.hillery@my.jcu.edu.au>
2019-07-14 13:36:40 +1000

    Fixed up null checks, added logger, added catchs, removed uneccesary code

--- ProjectSquishy/src/main/java/ProjectSquishy/utils/IO/AccessResource.java
+++ ProjectSquishy/src/main/java/ProjectSquishy/utils/IO/AccessResource.java
@@ -1,29 +1,35 @@
 package ProjectSquishy.utils.IO;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import org.apache.log4j.Logger;
+
+import java.io.*;
 import java.net.URL;
 
 public class AccessResource {
-    public File getFile(String fileName){
+
+    private Logger logger = org.apache.log4j.Logger.getLogger(AccessResource.class);
+
+    public File getFile(String fileName) {
         ClassLoader classLoader = getClass().getClassLoader();
         URL resource = classLoader.getResource(fileName);
-        if (resource == null) {
-            throw new IllegalArgumentException("file is not found!");
-        }else{
-            return  new File(resource.getFile());
-        }
+        return new File(resource.getFile());
     }
 
-    public void printFile(File file) throws IOException{
-        if (file == null) return;
-        try (FileReader reader = new FileReader(file);
-                BufferedReader br = new BufferedReader(reader)){
-            String line;
-            while ((line = br.readLine()) != null){
-                System.out.println(line);
+    public void printFile(File file) {
+        if (file != null) {
+            try {
+                FileReader reader = new FileReader(file);
+                BufferedReader br = new BufferedReader(reader);
+
+                while ((br.readLine()) != null) {
+                    System.out.println(br.readLine());
+                }
+
+            } catch (FileNotFoundException e) {
+                logger.error("File not found: " + file);
+
+            } catch (IOException e) {
+                logger.error("File cannot be read: " + file);
             }
         }
     }
